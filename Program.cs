94b52a56a9ab8bf7983a7f0f21229cf3a0b94892
Oluwasegun <akinpelu53@gmail.com>
2021-04-25 14:05:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("This program finds the remainder");
+            Console.WriteLine("Enter the Numerator: ");
+            double numerator=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Denominator");
+            double denominator=double.Parse(Console.ReadLine());
+            double modulus=numerator%denominator;
+            Console.WriteLine($"The remainder is {modulus}");
+            Console.WriteLine("Thank You!");
+        }
+    }
+}
